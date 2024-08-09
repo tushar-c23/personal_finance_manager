0fb57040cb7d4726d6fd499647d98ddade252188
Tushar Choudhary <tusharc.cse@gmail.com>
2024-08-09 19:09:38 +0530

    ADD: Transaction model

--- models.py
+++ models.py
@@ -1,5 +1,7 @@
-from sqlalchemy import Column, Integer, String
+from sqlalchemy import Column, Integer, String, Float, ForeignKey, Enum
 from .database import Base
+from sqlalchemy.orm import relationship
+import enum
 
 
 class User(Base):
@@ -9,3 +11,22 @@ class User(Base):
     email = Column(String, unique=True, index=True)
     name = Column(String)
     hashed_password = Column(String)
+
+    transactions = relationship("Transaction", back_populates="user")
+
+
+class TransactionType(str, enum.Enum):
+    debit = "debit"
+    credit = "credit"
+
+
+class Transaction(Base):
+    __tablename__ = "transactions"
+    id = Column(Integer, primary_key=True, index=True)
+    user_id = Column(Integer, ForeignKey("users.id"))
+    amount = Column(Float)
+    category = Column(String)
+    description = Column(String)
+    transaction_type = Column(Enum(TransactionType))
+
+    user = relationship("User", back_populates="transactions")
