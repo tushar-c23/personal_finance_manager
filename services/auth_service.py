21db2f3f644877177eb0e00ca26103363b4aa4ea
Tushar Choudhary <tusharc.cse@gmail.com>
2024-08-09 18:40:22 +0530

    ADD: Auth services authenticate user and access token generation

---
+++ services/auth_service.py
@@ -0,0 +1,33 @@
+from datetime import datetime, timedelta
+from jose import jwt
+from passlib.context import CryptContext
+from personal_finance_manager.models import User
+
+SECRET_KEY = "syfe"
+ALGORITHM = "HS256"
+ACCESS_TOKEN_EXPIRE_MINUTES = 30
+
+pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
+
+
+def verify_password(plain_password, hashed_password):
+    return pwd_context.verify(plain_password, hashed_password)
+
+
+def get_password_hash(password):
+    return pwd_context.hash(password)
+
+
+def authenticate_user(db, username: str, password: str):
+    user = db.query(User).filter(User.username == username).first()
+    if not user or not verify_password(password, user.hashed_password):
+        return False
+    return user
+
+
+def create_access_token(data: dict, expires_delta: timedelta):
+    to_encode = data.copy()
+    expire = datetime.utcnow() + expires_delta
+    to_encode.update({"exp": expire})
+    encoded_jwt = jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
+    return encoded_jwt
